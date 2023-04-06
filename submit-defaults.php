bf723d84a3ea9aae2d1e7557094aa6406b3b966d
ccatura <charlie@charliecatura.com>
2023-04-06 17:49:04 -0400

    tweaked succes message

--- submit-defaults.php
+++ submit-defaults.php
@@ -10,12 +10,12 @@ if(isset($_POST['submit']))
     $sql = "UPDATE `time_defaults` SET `sku_time`='$sku_time',`video_time`='$video_time',`ts_time`='$ts_time' WHERE 1";
 
     if (mysqli_query($conn, $sql)) {
-      echo "<div style='font-size:24px;'>";
+      echo "< style='font-size:24px;'>";
       echo "SKU time:   " . $sku_time . " minute(s)<br>";
       echo "Video time: " . $video_time . " minute(s)<br>";
       echo "360 time:   " . $ts_time . " minute(s)<br><br>";
       echo "Times have been added successfully!<br><br>";
-      echo "<a href='./defaults.php'>Go back to defaults.</a></div>";
+      echo "<a href='./defaults.php'>Go back to defaults.</a><br><br>";
       echo "<a href='./'>Go back to Photo Times Estimator app.</a></div>";
     } else {
        echo "Error: " . $sql . ":-" . mysqli_error($conn);
